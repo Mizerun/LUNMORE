ae1f2299a6361249c2e191aec5111c3da8169fe3
Mirai <caverypeople@gmail.com>
2024-04-22 14:37:03 +0700

    21312

--- js/script.js
+++ js/script.js
@@ -23,39 +23,3 @@ ScrollSmoother.create({
 	content: '.content'
 })
 
-document.getElementById("commentForm").addEventListener("submit", function(event) {
-  event.preventDefault();
-  
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const comment = document.getElementById("comment").value;
-  const photo = document.getElementById("preview").src;
-
-  const displayedComment = document.createElement("div");
-  displayedComment.classList.add("comment");
-  displayedComment.innerHTML = `
-      <strong>${name}</strong> (${email}): ${comment}
-      <br>
-      <img src="${photo}" alt="Uploaded photo" class="preview-img">
-  `;
-  
-  document.getElementById("comments").appendChild(displayedComment);
-
-  document.getElementById("commentForm").reset();
-  document.getElementById("preview").style.display = "none";
-});
-
-function previewImage(event) {
-  const preview = document.getElementById("preview");
-  const file = event.target.files[0];
-  const reader = new FileReader();
-
-  reader.onload = function() {
-      preview.src = reader.result;
-      preview.style.display = "block";
-  }
-
-  if (file) {
-      reader.readAsDataURL(file);
-  }
-}
